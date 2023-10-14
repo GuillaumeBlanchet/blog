b088f6ea161506d644fb0380fc524ba250e484f8
Guillaume Blanchet <guillaume.blanchet2@gmail.com>
2023-10-14 14:06:49 -0400

    add get-contact sync action

--- Salesforce/get-contact.js
+++ Salesforce/get-contact.js
@@ -2,19 +2,21 @@ var jsforce = require('jsforce');
 var conn = new jsforce.Connection({
     loginUrl : 'https://guillaumeblanchet-dev-ed.develop.my.salesforce.com'
 });
-var user = '';
-var pwd = '';
-var token = '';
-conn.login('', pwd+token, (err, res) => {
+console.log('Test env variables:');
+console.log(process.env.TEST);
+
+var pwd = process.env.SF_PWD;
+var token = process.env.SF_SECURITY_TOKEN;
+conn.login('guillaume.blanchet2@gmail.com', pwd+token, (err, res) => {
     // clear your synchronized database account table
     // example with firebase:
-    firebase.database().ref('account').remove();
+    //firebase.database().ref('account').remove();
     var query = conn.query('SELECT Id, Name FROM Account')
         .on('record', record => {
             console.log(record);
             // insert record into your database
             // example with firebase:
-            firebase.database().ref('account').push(record);
+            //firebase.database().ref('account').push(record);
         })
         .on('end', () => {
             console.log("total in database : " + query.totalSize);
